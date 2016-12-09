d2feac7e1707af8b64fb7c5bd2fc8b3b5841b77d
Cori Sparks (CASPAR9719) <CASPAR9719@ung.edu>
2016-12-09 16:33:06 -0500

    added trading functions

--- npc.py
+++ npc.py
@@ -1,4 +1,4 @@
-import items
+import game
 
 class Npc(object):
     def __init__(self):
@@ -11,12 +11,57 @@ class Trader(Npc):
     def __init__(self):
         self.name = "Shop Keeper"
         self.Money = 10000
-        self.backpack = [items.Coffee(),
-                         items.Coffee(),
-                         items.Bagle(),
-                         items.Bagle(),
-                         items.FlashDrive,
-                         items.Ramen()]
+        self.inventory = [game.Coffee(),
+                         game.Coffee(),
+                         game.Bagle(),
+                         game.Bagle(),
+                         game.FlashDrive,
+                         game.Ramen()]
         
-                         
+def trade(self, buyer, seller):
+    for i in item in enumerate(seller.inventory,1):
+        print("{}. {} - {} Money" .format(i,item.name, item.value))
+    while True:
+        user_input = raw_input ("Choose an Item or press X to exit: ").lower().strip()
+        if user_input == 'x':
+            return
+        else:
+            try:
+                choice = int(user_input)
+                to_swap = seller.inventory[choice-1]
+                self.swap(seller, buyer, to_swap)
+            except ValueError:
+                print("Invalid choice!")
+                
+def swap( self, seller, buyer, item):
+    if game.value > buyer.gold:
+        print("You don't have enough money for that!")
+        return
+    seller.inventory.remove(item)
+    buyer.inventory.append(item)
+    seller.gold = seller.gold + item.value
+    print ("Thank You come again!")
+
+def check_if_trade(self, player):
+    while True:
+        print ("Would you like to Buy(B), Sell(S), or Exit(X)?")
+        user_input = raw_input().lower.strip
+        if user_input == 'q':
+            return
+        elif user_input == 'b':
+            print ("Here is what you can buy: ")
+            self.trade(buyer=player, seller=self.Trader)
+        elif user_input == 's':
+            print ("Here is what you can sell: ")
+            self.trade(buyer=self.Trader, seller=self.player)
+        else:
+                       print ("Invalid Choice!")
+                       
+                       
+                       
+                   
+            
+    
+    
+                
     
